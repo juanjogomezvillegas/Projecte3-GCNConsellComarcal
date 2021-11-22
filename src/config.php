c9d14a52f266c2e709a89586646909ab49f59a64
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-11-22 16:42:19 +0100

    Primer Commit Projecte Nou

---
+++ src/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = array();
+
+/*Configuració de les dades per fer la connexió a la base de dades*/
+$config["user"] = "";
+$config["pass"] = "";
+$config["dbname"] = "";
+$config["host"] = "localhost";
