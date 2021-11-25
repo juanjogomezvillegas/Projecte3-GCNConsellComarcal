3fa29297c9c8d937e86d518a99baa5ed8c2ccc5e
moha <molamedb@gmail.com>
2021-11-25 13:49:54 -0500

    Arreglades rutes. Afegits paquets i afegit footer

--- src/view/portada.php
+++ src/view/portada.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/style.css">
+    <!-- Incluguem el fitxer head que contindrà totes  -->
+   <?php include '../src/includes/head.php';?>
     <title>Document</title>
 </head>
 <body>
 <h1>Lorem, ipsum.</h1>
 <h2>Lorem, ipsum dolor.</h2>
 <h3>Lorem ipsum dolor sit.</h3>
+<?php include '../src/includes/footer.php';?>
 </body>
 </html>
