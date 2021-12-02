4015846d9a53341396eaed78c91d2bfdc9d6dbc9
Luky7600 <alexaguilera202@gmail.com>
2021-12-02 19:10:25 +0100

    crear categorias i fer controladors per d'ur la creacio també he posat el sql en categoriesPDO per ger el registre

---
+++ src/view/crearcategoria.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<?php include '../src/includes/head.php'; ?>
+<title>Sing Up | GCN Consell Comarcal</title>
+</head>
+<body class="bg-black font-sans leading-normal tracking-normal">
+<?php
+include '../src/includes/nav_admin.php';
+?>
+<div class="min-w-screen min-h-screen flex items-center justify-center px-5 py-5 mt-20">
+    <div class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" style="max-width:1000px">
+        <div class="md:flex w-full">
+            <div class="w-full py-10 px-5 md:px-10">
+                <div class="text-center mb-10">
+                    <h1 class="font-bold text-3xl text-gray-900">CREAR CATEGORIA</h1>
+                </div>
+                <div>
+                    <form action="index.php?r=docrearcategoria" method="post">
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5">
+                            <div class="flex">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
+                                <input type="text" name="nom" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-red-500" placeholder="Nom Categoria">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5">
+                            <button type="submit" class="block w-full max-w-xs mx-auto bg-red-500 hover:bg-red-300 focus:bg-red text-white rounded-lg px-3 py-3 font-semibold">CREA CATEGORIA</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- src/view/llistarcategoria.php
+++ src/view/llistarcategoria.php
@@ -13,12 +13,17 @@
     include '../src/includes/nav_admin.php';
     ?>
 <div class="container w-full mx-auto pt-20">
-<div class="w-full px-4 md:px-0 md-8 mt-20 mb-16 text-gray-800 leading-normal">
+<div class="w-full px-4 md:px-0 md-8 mt-20 mb-16 text-gray-800 leading-normal text-center">
+<a href="index.php?r=crearcategoria">
+<button class="bg-red-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded inline-flex items-center mt-5">
+                <i class="fas fa-folder-plus"></i>
+                <span class="ml-1">Crear Categoria</span>
+            </button>
 <div class="flex flex-col">
-    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div>
         <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
             <div class="overflow-hidden sm:rounded-lg shadow-md">
-                <table class="table-auto">
+                <table class="mr-auto ml-auto mt-5">
                 <thead class="bg-gray-100 dark:bg-gray-700">
                         <tr>
                             <th scope="col" class="text-xs font-medium text-gray-700 px-6 py-3 text-left uppercase tracking-wider dark:text-gray-400">
@@ -39,9 +44,6 @@
                             </td>
                             <td class="text-sm text-gray-500 px-6 py-4 whitespace-nowrap dark:text-gray-400">
                                  <?= $actual['nom'];?>
-                            </td>
-                            <td class="text-sm text-gray-500 px-6 py-4 whitespace-nowrap dark:text-gray-400">
-                                 <?= $actual['cognom'];?>
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                             <a href="" class="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline">Editar</a>
