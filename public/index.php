a58d46513be5f816bbe8d1ad513f94f8a68311b3
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-02 19:44:39 +0100

    conflictes

--- public/index.php
+++ public/index.php
@@ -6,28 +6,6 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 require_once "../src/config.php";
 
 /*Afegim els controladors necessaris per executar tots els requests*/
-<<<<<<< HEAD
-include "../src/controller/portada.php";
-include "../src/controller/login.php";
-include "../src/controller/dologin.php";
-include "../src/controller/doregistre.php";
-include "../src/controller/docrearcategoria.php";
-include "../src/controller/doregistreadmin.php";
-include "../src/controller/registre.php";
-include "../src/controller/registreadmin.php";
-include "../src/controller/article.php";
-include "../src/controller/blog.php";
-include "../src/controller/tramit.php";
-include "../src/controller/admin.php";
-include "../src/controller/llistarusuari.php";
-include "../src/controller/llistararticle.php";
-include "../src/controller/llistarcategoria.php";
-include "../src/controller/crearcategoria.php";
-include "../src/controller/esborrarusuari.php";
-include "../src/controller/esborrararticle.php";
-include "../src/controller/esborrarcategoria.php";
-
-=======
 require_once "../src/controller/portada.php";
 require_once "../src/controller/login.php";
 require_once "../src/controller/dologin.php";
@@ -36,11 +14,9 @@ require_once "../src/controller/article.php";
 require_once "../src/controller/blog.php";
 require_once "../src/controller/tramit.php";
 require_once "../src/controller/admin.php";
->>>>>>> feature-wireframes
 
 $contenidor = new \Emeset\Contenidor($config);
 $app = new \Emeset\Emeset($contenidor);
-
 
 $app->ruta("", "ctrlPortada");
 $app->ruta("login", "ctrlLogin");
@@ -63,7 +39,5 @@ $app->ruta("article", "ctrlArticle");
 $app->ruta("blog", "ctrlBlog");
 $app->ruta("tramit", "ctrlTramit");
 $app->ruta("admin", "ctrlAdmin");
-
-
 
 $app->executa();
