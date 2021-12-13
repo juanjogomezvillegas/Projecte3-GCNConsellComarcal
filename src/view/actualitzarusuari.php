8b6c88dcf7a691778c6cdd33a581f977174dcbe5
Luky7600 <alexaguilera202@gmail.com>
2021-12-13 19:02:47 +0100

    canvio menor

--- src/view/actualitzarusuari.php
+++ src/view/actualitzarusuari.php
@@ -75,14 +75,6 @@ include '../src/includes/nav_admin.php';
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                <input type="password" name="confirm_password" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-red-500" placeholder="Confirmar Contrasenya">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex -mx-3">
-                        <div class="w-full px-3 mb-5">
                             <button type="submit" class="block w-full max-w-xs mx-auto bg-red-500 hover:bg-red-300 focus:bg-red text-white rounded-lg px-3 py-3 font-semibold">DESA ELS CANVIS</button>
                         </div>
                     </div>
