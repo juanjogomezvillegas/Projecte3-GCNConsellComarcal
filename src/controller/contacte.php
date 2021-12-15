f81aabe80a3e07280df8ba8a14a916ab34bd077e
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-15 16:26:36 +0100

    canvis menors

--- src/controller/contacte.php
+++ src/controller/contacte.php
@@ -2,11 +2,23 @@
 
 function ctrlContacte($peticio, $resposta, $contenidor)
 {
-    $error = $peticio->get(INPUT_GET, "error");  
+    $usuarisPDO = $contenidor->usuarisPDO();
+
+    $usuarilogat = $peticio->get(INPUT_COOKIE, "usuarilogat");
+    $logat = $peticio->get("SESSION", "logat");
+    $error = $peticio->get(INPUT_GET, "error");
 
     if(!empty($error)){
         $resposta->set("error", $error);
     }
+
+    if ($logat) {
+        $dadesUsuari = $usuarisPDO->get($usuarilogat);
+
+        $resposta->set("dadesUsuari", $dadesUsuari);
+    }
+
+    $resposta->set("logat", $logat);
     
     $resposta->SetTemplate("contacte.php");
 
