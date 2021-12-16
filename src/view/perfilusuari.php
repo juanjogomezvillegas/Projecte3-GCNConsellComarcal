9db62320cf167fd87d494f5c9d816b5ae8654edc
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-16 13:14:08 +0100

    conflictes

--- src/view/perfilusuari.php
+++ src/view/perfilusuari.php
@@ -23,7 +23,7 @@
         </div>
         <div class="text-center">
                     <a href="index.php?r=">
-                        <button class="bg-red-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded inline-flex items-center mt-5">
+                        <button class="bg-red-600 hover:bg-red-300 text-white font-bold py-2 px-4 rounded inline-flex items-center mt-5">
                             <i class="fas fa-camera" aria-hidden="true"></i>
                             <span class="ml-1">Canviar Imatge</span>
                         </button>
@@ -32,35 +32,35 @@
         <div class="md:col-span-3 h-48 p-4 space-y-2 p-3">
                 <div class="flex ">
                     <span
-                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Name:</span>
+                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Nom i Cognom:</span>
                     <input 
                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
                         type="text" value="<?=$dadesUsuari["nom"];?> <?=$dadesUsuari["cognom"];?>"  readonly/>
                 </div>
                 <div class="flex ">
                     <span
-                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Username:</span>
+                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Nom d'Usuari (Username):</span>
                     <input 
                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
                         type="text" value="<?=$dadesUsuari["username"];?>"  readonly/>
                 </div>
                  <div class="flex ">
                     <span
-                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6"> Email:</span>
+                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6"> Correu Electrónic:</span>
                     <input 
                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
                         type="text" value="<?=$dadesUsuari["email"];?>"  readonly/>
                 </div>
                 <div class="flex ">
                     <span
-                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6"> Telefon:</span>
+                        class="text-sm border bg-red-500 font-bold text-white uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6"> Teléfon:</span>
                     <input 
                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
                         type="text" value="<?=$dadesUsuari["telefon"];?>"  readonly/>
                 </div>
                 <div class="text-center">
                     <a href="index.php?r=">
-                        <button class="bg-red-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded inline-flex items-center mt-5">
+                        <button class="bg-red-600 hover:bg-red-300 text-white font-bold py-2 px-4 rounded inline-flex items-center mt-5">
                             <i class="fas fa-unlock-alt" aria-hidden="true"></i>
                             <span class="ml-1">Canviar Contrasenya</span>
                         </button>
