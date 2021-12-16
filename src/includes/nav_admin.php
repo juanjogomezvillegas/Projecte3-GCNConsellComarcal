74a746daa92a480c850050a0092a7777080b5634
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-16 12:58:22 +0100

    canvis menors

--- src/includes/nav_admin.php
+++ src/includes/nav_admin.php
@@ -74,6 +74,11 @@
                             <i class="fas fa-wallet fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Categories</span>
                         </a>
                     </li>
+					<li class="mr-6 my-2 md:my-0">
+                        <a href="index.php?r=llistarmissatges" class="block py-1 md:py-3 pl-1 align-middle text-gray-50 no-underline hover:text-gray-200  border-gray-50  hover:border-blue-200">
+                            <i class="fas fa-envelope fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Missatges</span>
+                        </a>
+                    </li>
 				</ul>
 				
 			
