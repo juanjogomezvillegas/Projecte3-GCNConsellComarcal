3b4c97d390cae26f159332effde4f0954e55eb2d
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-21 12:00:29 +0100

    l'slaider d'imatges ja funciona correctament

--- src/controller/doactualitzarslider.php
+++ src/controller/doactualitzarslider.php
@@ -10,13 +10,8 @@ function ctrlDoActualitzarSlider($peticio, $resposta, $contenidor)
     $idslider = filter_var($idslider2, FILTER_SANITIZE_NUMBER_INT);
 
     if (isset($imatgeslider["name"]) && ($imatgeslider["type"] === "image/png" || $imatgeslider["type"] === "image/jpeg")) {
-        if ($imatgeslider["type"] === "image/png") {
-            $nomficher = $idslider2 . ".png";
-        } else {
-            $nomficher = $idslider2 . ".jpg";
-        }
-        $slidersPDO->updateImage($idslider, $nomficher);
-        move_uploaded_file($imatgeslider["tmp_name"], "img/slider/" . $nomficher);
+        $slidersPDO->updateImage($idslider, $imatgeslider["name"]);
+        move_uploaded_file($imatgeslider["tmp_name"], "img/slider/" . $imatgeslider["name"]);
     }
 
     $resposta->redirect("Location:index.php?r=sliders");
